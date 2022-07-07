ba0431b2588df8a9dab4eaabf1c5aa14297c86bf
WestSilver <seo606101@naver.com>
2022-07-07 22:29:41 +0900

    test

---
+++ src/java0623/java0623.java
@@ -0,0 +1,34 @@
+package java0623;
+
+import java.util.Scanner;
+
+public class java0623 {
+
+	public static void main(String[] args) {
+		Scanner sc=  new Scanner(System.in);
+		System.out.println("첫번재 수");
+		int x = sc.nextInt();
+		System.out.println("첫번재 수"+x);
+		System.out.println("두번째 수");
+		int y = sc.nextInt();
+		System.out.println("두번째 수"+y);
+	
+		
+		String s1 = "Hello";
+		String s2 = "World!";
+	
+;
+		int sum;
+
+		sum = x+y;  
+	
+		boolean flag = true;
+		flag = 100>200;
+
+		System.out.println("두 수의 합=" + sum);
+		System.out.println(s1+s2);
+		System.out.println(flag);
+		
+		
+	}
+}
